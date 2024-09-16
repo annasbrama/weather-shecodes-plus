b0ef2cdcbdd56a48ae05d5c62e2557a2e6fc13e3
Anna <acesbrama@gmail.com>
2024-09-16 17:11:16 -0300

    API adicionada e usando axios

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
-let input = document.querySelector('.input')
-let button = document.querySelector('.btn-search')
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector(".weather");
+    let temperature = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector(".current-city");
 
-button.addEventListener('click', cities)
-
-function cities() {
-    event.preventDefault()
-    const city = input.value.trim()
-    const weatherCity = document.querySelector('.weather-cities')
-
-    //DATETIME
-    const currentDate = new Date()
-    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][currentDate.getDay()]
-    const hours = String(currentDate.getHours()).padStart(2, '0')
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0')
-
-    //CITIES NAMES
-    const cityParts = city.split(' ')
-    const correctedCity = cityParts.map(part => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()).join(' ')
-
-    if (correctedCity !== '') {
-
-        weatherCity.innerHTML = `<h1>${correctedCity}</h1>
-                    <p>
-                    <span class="datetime">${dayOfWeek} ${hours}:${minutes}</span>, few clouds <br>
-                        Humidity: <span>74%</span>, Wind <br>
-                        <span>11.32km/h</span>
-                    </p>`
-    } else {
-        alert(
-            `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-        );
-    }
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = `${temperature}°C`;
 }
+
+
+function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector(".input");
+    let city = searchInputElement.value;
+
+    let apiKey = "";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector('form');
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector(".datetime");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
